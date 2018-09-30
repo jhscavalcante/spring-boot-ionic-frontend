3321cc9fc3c624ea8074c243e7ac727d65574a98
Henrique Cavalcante <=>
2018-09-30 07:34:03 -0300

    Salvando os dados do usuário logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -37,7 +37,8 @@ export class HomePage {
     
     this.auth.authenticate(this.creds)
     .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        //console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
         this.navCtrl.setRoot('CategoriasPage');
       },
       error =>{});       
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from "@angular/core";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService{
 
-    constructor(public http: HttpClient){        
+    constructor(public http: HttpClient, public storage: StorageService){        
     }
 
     authenticate(creds: CredenciaisDTO){
@@ -17,5 +19,19 @@ export class AuthService{
                 observe: 'response', // vai retornar um objeto do tipo response (desta forma consigo acessar o header)
                 responseType: 'text' // do tipo text pq o body retorna vazio, desta forma não tenta fazer o casting para JSON, ou seja impede o erro de parse 
             });
+    }
+
+    // Recebe o Beader Token do cabeçalho da resposta
+    successfulLogin(authorizationValue : string){
+        let tok = authorizationValue.substring(7); // recorta a string a partir do 7° caractere
+        let user: LocalUser = {
+            token: tok
+        };
+
+        this.storage.setLocalUser(user);
+    }
+
+    logout(){
+        this.storage.setLocalUser(null);
     }
 }
